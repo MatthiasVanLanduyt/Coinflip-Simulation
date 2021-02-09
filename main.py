df9f9e4828530fc9c3bcb3e932cd439231a11a5d
Matthias Van Landuyt <matthias.vanlanduyt@gmail.com>
2021-02-09 20:10:01 +0100

    code for one simulation ok

--- main.py
+++ main.py
@@ -7,25 +7,31 @@ def coinflip():
     random.random()
     flip = random.choice(sides)
     
-    print('Flip: ' + flip)
+    # print('Flip: ' + flip)
 
     return flip
 
 def simulate_coinflips (n):
     results_sequence = []
     results = {'Heads': 0, 'Tails': 0}
-    results_percentage = 
     for index in range (n):
         flip = coinflip()
         results_sequence.append(flip)
         results[flip] += 1
-        print (results)
+        
+  
+    heads_percentage = results['Heads'] / n * 100
+
+    print(f'Summarized data: {results}')
+    print(f'Heads percentage: {heads_percentage}%')
 
 def play():
     while True:
         try:
             n = int(input('How many simulations do you want to run?'))
+            print(n)
             simulate_coinflips(n)
+            print('All done')
             break
         except:
             print('Ooops....we have some errors.')
